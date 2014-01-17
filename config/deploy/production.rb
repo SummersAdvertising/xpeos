a9bcc6ef8f454f25cc12d56850c33950b2a0a69a
Yuzhe <yuzhe.c85@gmail.com>
2014-01-17 19:51:07 +0800

    adjust

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,9 +5,9 @@ set :stage, :production
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deploy@bye-xp.cloudapp.net}
+role :web, %w{deploy@bye-xp.cloudapp.net}
+role :db,  %w{deploy@bye-xp.cloudapp.net}
 
 # Extended Server Syntax
 # ======================
@@ -15,7 +15,7 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'bye-xp.cloudapp.net', user: 'deploy', password: '1qaz2wsx', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
@@ -40,3 +40,17 @@ server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 # setting per server overrides global ssh_options
 
 # fetch(:default_env).merge!(rails_env: :production)
+
+
+desc "Create database.yml and asset packages for production"
+before "deploy:finishing", :set_database do
+	on roles( :app ), in: :sequence do
+		db_config = "#{shared_path}/config/database.yml.production"
+		#db_config = "#{db_config} #{release_path}/config/database.yml.production"
+		execute :cp, "#{shared_path}/config/database.yml.production #{release_path}/config/database.yml"
+		within current_path do
+			execute :bundle, "install --deployment"
+			execute :rake, "assets:precompile RAILS_ENV=production"
+		end
+	end
+end
