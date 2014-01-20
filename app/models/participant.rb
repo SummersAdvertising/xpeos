f9db54b9bb9faa265032b3209a6aa174d3932145
Yuzhe <yuzhe.c85@gmail.com>
2014-01-20 15:45:08 +0800

    add form style

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -8,7 +8,9 @@ class Participant < ActiveRecord::Base
 	validates_format_of :email, :with => /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i, :message => "請輸入正確的電子信箱格式"
 	
 	validates_presence_of :address, :message => '請輸入您的住址'
-	validates_presence_of :key, :message => '請輸入您購買的產品序號'
+	validates_presence_of :key, :message => '請輸入您購買的產品識別碼'
+	validates_format_of :key, :with => /\A\w{5}\-\w{5}\-\w{5}\-\w{5}\z/i, :message => '產品識別碼格式有誤，請重新檢查'
+	
 	validates_presence_of :product, :message => '請勾選您購買的產品類別'
 	validates_presence_of :extra, :message => '請勾選購買廠牌'
 	validates_presence_of :extra_product, :message => '請輸入購買的產品型號'
